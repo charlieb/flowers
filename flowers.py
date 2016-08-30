04768928397d8560480739c7ab92665ec2055396
Charlie Burrows <charlie.burrows@gmail.com>
2016-08-30 00:53:17 -0400

    Added PhiFlower

--- flowers.py
+++ flowers.py
@@ -21,7 +21,7 @@ def circle_points(cx,cy,r,point_separation=None,npoints=None, close=True):
         points.append([cx + r*cos(a), cy + r*sin(a)])
     return points
 
-class Flower:
+class PetalLayer:
     def __init__(self, pistil_radius=1., petal_length=1., num_petals=6):
         self.pistil_radius = pistil_radius
         self.petal_length = petal_length
@@ -29,24 +29,25 @@ class Flower:
         self.paths = []
         self.petal_centers = []
 
+        self.calc_paths()
+        self.remove_occluded()
+
     def calc_paths(self):
-        self.paths=[circle_points(0.,0., self.pistil_radius, point_separation=2.)]
         self.petal_centers = circle_points(0.,0.,
                                     self.pistil_radius + self.petal_length / 2.,
                                     npoints=self.num_petals,
                                     close=False)
         petal_radius = self.petal_length / 2.
-        for pc in self.petal_centers:
-            self.paths.append(circle_points(pc[0],pc[1], petal_radius, point_separation=2.))
+        self.paths = [circle_points(pc[0],pc[1], petal_radius, point_separation=2.) for pc in self.petal_centers]
 
     def remove_occluded(self):
         petal_radius = self.petal_length / 2.
         # petal point and center, and the petal it's overlapping.
         for ci, pc in enumerate(self.petal_centers):
-            # ignore up to current path (ci+1) and pistil path (+1)
+            # ignore up to current path (ci+1)
             # and go round twice but make sure updates to the first instance of
             # paths are reflected in the second (shallow copy)
-            for move_overlaps in self.paths[ci+2:]+self.paths[1:ci+2]:
+            for move_overlaps in self.paths[ci+1:]+self.paths[1:ci+1]:
                 overlap_points = [] # contains data about all the overlapping points
                 for i, p in enumerate(move_overlaps):
                     dx = p[0] - pc[0]
@@ -81,18 +82,28 @@ class Flower:
                 else: # the first time one doesn't overlap stop looking
                     break 
 
+class Flower:
+    def __init__(self, pistil_radius=1.):
+        self.pistil_radius = pistil_radius
+        self.petal_layers = []
+        self.pistil = circle_points(0.,0., self.pistil_radius, point_separation=2.)
+
+    def add_petal_layer(self, petal_length, num_petals):
+        self.petal_layers.append(PetalLayer(self.pistil_radius, petal_length, num_petals))
+
     def draw(self):
-        self.calc_paths()
-        print(len(self.paths))
-        self.remove_occluded()
-        print(len(self.paths))
 
         dwg = svgwrite.Drawing('test%05i.svg'%0, profile='tiny')
         line_width = 1
         minx = miny =  9999999
         maxx = maxy = -9999999
-        cols = ['red','green','blue'] * len(self.paths)
-        for p, col in zip(self.paths, cols):
+        paths = [self.pistil]
+        for p in self.petal_layers:
+            paths += p.paths
+
+        cols = ['red','green','blue'] * len(paths)
+
+        for p, col in zip(paths, cols):
             for (x,y) in p:
                 minx = x if x < minx else minx
                 maxx = x if x > maxx else maxx
@@ -108,35 +119,58 @@ class Flower:
                     width=maxx-minx+2*line_width, height=maxy-miny+2*line_width)
         dwg.save()
 
+class PhiFlower:
+    def __init__(self, pistil_radius=1.):
+        self.pistil_radius = pistil_radius
+        self.petals = []
+        self.angle = 0
+        self.point_separation = 2.
+        self.pistil = circle_points(0.,0., self.pistil_radius, self.point_separation)
+        self.phi = (1 + sqrt(5)) / 2
 
-def draw(starting_points, npoints, points, connections, frame):
-    dwg = svgwrite.Drawing('test%05i.svg'%frame, profile='tiny')
-    minx = miny =  9999999
-    maxx = maxy = -9999999
-    line_width = 1
-    for i in range(npoints):
-        minx = points[i][0] if points[i][0] < minx else minx
-        maxx = points[i][0] if points[i][0] > maxx else maxx
-        miny = points[i][1] if points[i][1] < miny else miny
-        maxy = points[i][1] if points[i][1] > maxy else maxy
-    dwg.viewbox(minx=minx-line_width, miny=miny-line_width, 
-                width=maxx-minx+2*line_width, height=maxy-miny+2*line_width)
-    # reverse starting points so that if concentric the largest one
-    # will be first so that when they're drawn with fill they'll all be 
-    # visible
-    rev_starts = starting_points.copy()
-    rev_starts.reverse()
-    #fills = ['green', 'blue', 'red'] * len(starting_points) # far too long but meh
-    fills = ['none'] * len(starting_points)
-    for start, fill in zip(rev_starts, fills):
-        svgpath = svgwrite.path.Path(path(start, points, connections), fill=fill)
-        svgpath.push('Z')
-        svgpath.stroke('black', width=1)
-        dwg.add(svgpath)
-    dwg.save()
+    def add_petals(self, n, petal_radius):
+        for _ in range(n):
+            cx = (self.pistil_radius + petal_radius) * cos(self.angle)
+            cy = (self.pistil_radius + petal_radius) * sin(self.angle)
+            self.petals.append(circle_points(cx,cy, petal_radius, self.point_separation))
+            self.angle += self.phi
+
+    def draw(self):
+        dwg = svgwrite.Drawing('test%05i.svg'%0, profile='tiny')
+        line_width = 0.1
+        minx = miny =  9999999
+        maxx = maxy = -9999999
+
+        paths = self.petals + [self.pistil]
+        cols = ['red','green','blue'] * len(paths)
+        cols = ['black'] * len(paths)
+
+        for p, col in zip(paths, cols):
+            for (x,y) in p:
+                minx = x if x < minx else minx
+                maxx = x if x > maxx else maxx
+                miny = y if y < miny else miny
+                maxy = y if y > maxy else maxy
+
+            if len(p) > 0:
+              pline = svgwrite.shapes.Polyline(p, fill='white')
+              dwg.add(pline)
+              #svgpath.stroke('black', width=line_width)
+              pline.stroke(col, width=line_width)
+        dwg.viewbox(minx=minx-line_width, miny=miny-line_width, 
+                    width=maxx-minx+2*line_width, height=maxy-miny+2*line_width)
+        dwg.save()
+
+
 
 def main():
-    f = Flower(pistil_radius=10, petal_length=10, num_petals=5)
+    #f = Flower(pistil_radius=10)
+    #f.add_petal_layer(petal_length=20, num_petals=6)
+#    f.add_petal_layer(petal_length=15, num_petals=15)
+#    f.add_petal_layer(petal_length=10, num_petals=25)
+    f = PhiFlower(10)
+    for i in range(30):
+        f.add_petals(5, 20 - 15*i / 30)
     f.draw()
 
 if __name__ == '__main__':
